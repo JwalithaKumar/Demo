2e0cb5d0cd1fee6483dc0740cd30e9adbc504ae9
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2023-12-29 11:46:35 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
 import streamlit as st
 st.set_page_config(page_title = 'cats')
 st.markdown("## Types of Cats")
-st.image("./Persian.jpg")
 st.write("- Persian Cat")
-st.image("./ragdoll.jpg")
+st.image("./Persian.jpg")
 st.write("- Ragdoll Cat")
+st.image("./ragdoll.jpg")
+st.write("- Burmese cat")
+st.image("https://d3544la1u8djza.cloudfront.net/APHI/Blog/2021/07-08/brown+Burmese+kitten+lying+on+a+cream+couch-min.jpg")
 
