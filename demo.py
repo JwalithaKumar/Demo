8f351d9b6bc9ea3ee0ef698a28670de3809c8ec5
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2023-12-29 11:58:10 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
 import streamlit as st
-
 st.set_page_config(page_title='Cats')
 st.header("Types of Cats")
 st.subheader("Persian Cat")
 st.image("./Persian.jpg", caption="Persian Cat", width=300)
 st.subheader("Ragdoll Cat")
-st.image("./Ragdoll.jpg", caption="Ragdoll Cat", width=300)
+st.image("./ragdoll.jpg", caption="Ragdoll Cat", width=300)
 
